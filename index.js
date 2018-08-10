405d0ed2216fa0820229ad3a2ccd2f01e8177f57
Max Yao <bojinyao@berkeley.edu>
2018-08-10 02:17:42 -0700

    app is working with consolidated configuration!

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const fs = require("fs");
-const utils = require("./utils.js");
-const customer = require("./bamazonCustomer.js");
-const manager = require("./bamazonManager.js");
-const supervisor = require("./bamazonSupervisor.js");
+const mysql = require("mysql");
+const colors = require("colors");
 const inquirer = require("inquirer");
 
+const utils = require("./utils.js");
+
+
 const DOTENV = ".env";
+const TABLES_SETUP_SQL = "./tables_setup.sql";
+
+//---------- Color Themes -------------
+colors.setTheme(utils.colorTheme);
+
+
+function configApp(fn = null) {
+    let configured = utils.fileExists(`./${DOTENV}`);
+    if (configured) {
+        let isParsed = require("dotenv").config();
+        if (isParsed.error) {
+            console.log(`Failed to parse .env File!`.error);
+            return;
+        }
+        if (fn) {
+            fn();
+        }
+    } else {
+        console.log(`Welcome New User!`.success);
+        console.log(`Please follow the following prompts to configure the app!`.success);
+        console.log(`Make sure you have a MySQL server running!!`.success);
+        console.log(`Result of your configuration can be found in .env file`.success);
+        console.log(`You can make future configuration changes there!`.success)
+        configPrompts(fn);
+    }
+}
+
 
 function configPrompts(fn = null) {
     inquirer.prompt([
@@ -31,46 +59,68 @@ function configPrompts(fn = null) {
         },
         {
             type: 'input',
-            message: `Name of Database to create:`,
+            message: `Database to create (WILL OVER-WRITE EXISTING!):`,
             name: 'database'
         }
-    ]).then(function(answer) {
+    ]).then(function (answer) {
         let host = answer.host.trim();
-        let port = answer.port.trim();
+        let port = answer.port;
         let user = answer.user.trim();
         let pswd = answer.pswd.trim();
         let database = answer.database.trim();
-        fs.appendFileSync(`./${DOTENV}`, `DB_HOST=${host}`);
-        fs.appendFileSync(`./${DOTENV}`, `DB_PORT=${port}`);
-        fs.appendFileSync(`./${DOTENV}`, `DB_USER=${user}`);
-        fs.appendFileSync(`./${DOTENV}`, `DB_PASS=${pswd}`);
-        fs.appendFileSync(`./${DOTENV}`, `DB_NAME=${database}`);
+        fs.appendFileSync(`./${DOTENV}`, `DB_HOST=${host}\n`);
+        fs.appendFileSync(`./${DOTENV}`, `DB_PORT=${port}\n`);
+        fs.appendFileSync(`./${DOTENV}`, `DB_USER=${user}\n`);
+        fs.appendFileSync(`./${DOTENV}`, `DB_PASS=${pswd}\n`);
+        fs.appendFileSync(`./${DOTENV}`, `DB_NAME=${database}\n`);
+
+        let isParsed = require("dotenv").config();
+        if (isParsed.error) {
+            console.log(`Failed to parse .env File!`.error);
+            return;
+        }
 
         let connection = mysql.createConnection({
             multipleStatements: true,
-            host: 'localhost',
-            user: 'root',
-            password: 'root'
+            host: host,
+            port: port,
+            user: user,
+            password: pswd
         });
         connection.query(
-            ``
+            `DROP DATABASE IF EXISTS ${database};
+            CREATE DATABASE ${database};`,
+            function (error, result, field) {
+                if (error) {
+                    console.log(`Failed configuration:`.error);
+                    console.log(error);
+                    return;
+                }
+                const stream = fs.createReadStream(TABLES_SETUP_SQL);
+                stream.on('data', (data) => {
+                    connection.query(`USE ${database}; ${data.toString('utf8')}`,
+                        function (error, result, field) {
+                            if (error) {
+                                console.log(`Failed to set up tables:`.error);
+                                console.log(error);
+                                return;
+                            }
+                        })
+                    connection.end(() => {
+                        console.log(`\nAll set up! App ready to use!\n`.success.bold);
+                        if (fn) {
+                            fn();
+                        }
+                    });
+                });
+            }
         )
     })
 }
 
-/**
- * Starting point of the entire App.
- */
-function Main() {
-
-    let configured = utils.fileExists(`./${DOTENV}`);
-    if (configured) {
-        require("dotenv").config();
-    } else {
-
-    }
 
 
+function roleSelection() {
     inquirer.prompt([
         {
             type: 'list',
@@ -81,14 +131,17 @@ function Main() {
         let role = answer.role;
         switch (role) {
             case 'Customer':
+                const customer = require("./bamazonCustomer.js");
                 customer();
                 break;
 
             case 'Manager':
+                const manager = require("./bamazonManager.js");
                 manager();
                 break;
 
             case 'Supervisor':
+                const supervisor = require("./bamazonSupervisor.js");
                 supervisor();
                 break;
             default:
@@ -97,7 +150,15 @@ function Main() {
     })
 }
 
+/**
+ * Starting point of the entire App.
+ */
+function Main() {
+    configApp(roleSelection);
+}
+
 //===================
 //Start of Entire App
 //===================
 Main();
+
